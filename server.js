22c197da375b1b07b860e655b57db18d2981a842
Eoin Gaughran <gaughraneoin@yahoo.ie>
2022-06-08 01:36:23 +0200

    new articles list working

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog')
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log("The error swey: " + err))
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: Date.now(),
-        description: 'Test description'
-    }]
-    res.render('index', { articles: articles })
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
