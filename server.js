06b680bee3223c39021a7b05d13bc9a7abb692d0
Eoin Gaughran <gaughraneoin@yahoo.ie>
2022-06-09 01:39:51 +0200

    slug & delete button

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog')
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log("The error swey: " + err))
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({ createdAt: 'desc' })
